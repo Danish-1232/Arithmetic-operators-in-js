7d3e5f06a80296262c2e4a3df0e574e30dd5c7eb
Danish-Farooqui <danishfarooqui303@gmail.com>
2024-11-11 15:22:24 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,27 @@
+// NO 1
+
+var sum = 5+3;
+console.log(sum);
+
+// NO 2
+
+var divide = 500/10;
+console.log(divide);
+
+// NO 3
+
+var product = 5*4;
+console.log(product);
+
+//  NO 4
+
+
+var sum = 20-2;
+console.log(sum);
+
+
+// NO 5
+
+var reminder = 5 % 3;
+console.log(reminder);
+
